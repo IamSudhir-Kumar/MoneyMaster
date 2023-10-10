c572e1599dd80be2a597dc4b994325b7d64fbb35
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-10-10 17:32:43 +0530

    routing start

---
+++ src/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Router } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+
+const Layout = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Layout
